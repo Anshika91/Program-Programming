96edd202c2e6b396342ec5a3179cfeefbffee58b
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-01-29 20:02:42 +0530

    Create Space_cycle.cpp
    added a solution

---
+++ Space_cycle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Moon" << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Star" << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Planet" << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << "Asteroid" << endl;
+    }
+    return 0;
+}
