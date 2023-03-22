80c463de539e1bd83a75ddc55a9a1bc79cd37f42
Anshika Mishra <92078327+Anshika91@users.noreply.github.com>
2023-03-22 20:45:09 +0530

    Create Candy Division.cpp
    added a codechef solution

---
+++ Candy Division.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int anshii;
+	cin>>anshii;
+	while(anshii--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n%3==0)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
